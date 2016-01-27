781dca2178d3832d6417811204c2f3bfa5a5cfdc
Tomasz-Silpion <tom@leftcurlybracket.com>
2016-01-27 20:58:24 +0100

    Added support for attachments thumbnails/icons and Imagick for pdf covers

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Magento attachments plugin with attachment type and multistore support
  
 ## Version
 
-###1.1.2
+###1.1.3
 
 ## Changelog
 
@@ -19,3 +19,7 @@ Magento attachments plugin with attachment type and multistore support
 ### 1.1.2
 
 * Added optional type of content display for categories
+
+### 1.1.3
+
+* Added support for attachments thumbnails/icons and Imagick support for pdf covers
--- app/code/community/LCB/Attachments/Model/Attachment.php
+++ app/code/community/LCB/Attachments/Model/Attachment.php
@@ -9,11 +9,25 @@
  */
 class LCB_Attachments_Model_Attachment extends Mage_Core_Model_Abstract {
 
+    protected $_absolutePath;
+    protected $_urlPath;
+    protected $_supportedImages = array(
+        'gif',
+        'jpg',
+        'jpeg',
+        'png'
+    );
+
     protected function _construct()
     {
+        $mediaUrl = Mage::app()->getStore(Mage::app()->getStore())->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA);
+        $this->_urlPath = $mediaUrl . "attachments";
+        $this->_absolutePath = Mage::getBaseDir('media') . DS . "attachments";
+        $this->_enableImagick = Mage::getStoreConfig('attachments/general/imagick');
+        $this->_defaultImage =  $mediaUrl . 'catalog' . DS . 'product' . DS . 'watermark' . DS . Mage::getStoreConfig('attachments/general/thumbnail');
         $this->_init("lcb_attachments/attachment");
     }
-
+    
     /**
      * Get attachment url
      * 
@@ -21,9 +35,51 @@ class LCB_Attachments_Model_Attachment extends Mage_Core_Model_Abstract {
      */
     public function getUrl()
     {
-        return Mage::app()->getStore(Mage::app()->getStore())->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA) . "attachments" . DS . $this->getFile();
+        return $this->_urlPath . DS . $this->getFile();
     }
-    
+
+    /**
+     * Get attachment image
+     * 
+     * @return string Image url
+     */
+    public function getImage()
+    {
+
+        $extension = pathinfo($this->getFile(), PATHINFO_EXTENSION);
+
+        if ($this->_enableImagick && $this->getExtension() == "pdf" && class_exists('Imagick')) {
+
+            $filename = substr($this->getFile(), 0, -strlen($extension)) . 'jpg';
+
+            if (!file_exists($this->_absolutePath . DS . $filename)) {
+                $imagick = new Imagick($this->getPath() . '[0]');
+                $imagick->setImageFormat('jpeg');
+                $imagick->writeImage($this->_absolutePath . DS . $filename);
+                $imagick->clear();
+                $imagick->destroy();
+            }
+
+            return $this->_urlPath . DS . $filename;
+        }
+
+        if (in_array($extension, $this->_supportedImages)) {
+            return $this->getUrl();
+        }
+        
+        return $this->_defaultImage;
+    }
+
+    /**
+     * Alias for getImage()
+     *
+     * @return string Image url
+     */
+    public function getThumbnail()
+    {
+        return $this->getImage();
+    }
+
     /**
      * Get attachment absolute url
      * 
@@ -47,6 +103,7 @@ class LCB_Attachments_Model_Attachment extends Mage_Core_Model_Abstract {
     /**
      * Get attachment extension
      * 
+     * @todo database replacement for performance
      * @return string file extension
      */
     public function getExtension()
